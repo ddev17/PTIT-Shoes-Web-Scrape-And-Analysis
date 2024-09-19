128a8f74823989a41d3baeb03bf80602f6e4819f
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-09-19 15:26:09 +0700

    update crawl adidas fields

--- crawl_adidas.py
+++ crawl_adidas.py
@@ -26,19 +26,43 @@ def get_data(product_link):
         soup = BeautifulSoup(response.text, 'html.parser')
         scripts = soup.find_all('script')
 
-        for i in range(len(scripts)):
-            if 'window.REACT_QUERY_DATA' in scripts[i].text and 'usps' in scripts[i].text:
-                index = scripts[i].text.find('{')
-                txt = scripts[i].text[index:]
-                data = json.loads(txt)
-                data = data['queries'][0]['state']['data']
+        try:
+            result_data = dict()
+            for i in range(len(scripts)):
+                if 'window.REACT_QUERY_DATA' in scripts[i].text and 'usps' in scripts[i].text:
+                    index = scripts[i].text.find('{')
+                    txt = scripts[i].text[index:]
+                    data = json.loads(txt)
+                    data = data['queries'][0]['state']['data']
 
-                price = data['pricing_information']['currentPrice']
-                gender = data['attribute_list']['gender']
-                desc = data['product_description']['usps']
-                desc = json.dumps(desc)
+                    price = data['pricing_information']['currentPrice']
+                    gender = data['attribute_list']['gender']
+                    usps = data['product_description']['usps']
+                    usps = json.dumps(usps, ensure_ascii=False)
+                    attribute_list = data['attribute_list']
+                    attribute_list = json.dumps(
+                        attribute_list, ensure_ascii=False)
+                    variation_list = data['variation_list']
+                    variation_list = json.dumps(variation_list)
 
-                return {'price': price, 'gender': gender, 'desc': desc}
+                    result_data['price'] = price
+                    result_data['gender'] = gender
+                    result_data['usps'] = usps
+                    result_data['attribute_list'] = attribute_list
+                    result_data['variation_list'] = variation_list
+                    break
+            # pprint(scripts[0].text)
+            data = json.loads(scripts[0].text)
+            result_data['color'] = data['color']
+            result_data['rating'] = data['aggregateRating']['ratingValue']
+            result_data['description'] = data['description']
+            result_data['name'] = data['name']
+            result_data['material'] = data['material']
+
+            pprint(result_data)
+            return result_data
+        except:
+            return {}
     else:
         print("Status code 403")
         return {}
@@ -48,13 +72,20 @@ def get_data(product_link):
 
 datas = []
 try:
-    with open('data', 'rb') as f:
+    with open('data.pkl', 'rb') as f:
         datas = pickle.load(f)
 except:
     pass
 
 for i in range(len(datas), len(product_links)):
-    datas.append(get_data(product_links[i]))
+    data = get_data(product_links[i])
 
-    with open('data', 'wb') as f:
+    while not bool(data):
+        print("Error, try again...")
+        data = get_data(product_links[i])
+
+    datas.append(data)
+
+    break
+    with open('data.pkl', 'wb') as f:
         pickle.dump(datas, f)
